aab9c1fa0daa86ca5f5ffe234e0c8d24ac2358e9
africa-ax <africashyaka5@gmail.com>
2025-12-04 13:04:02 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB5uSxd29wVSWt65IQsfVBo86IQG234Nbo",
+  authDomain: "smartebm-8ea35.firebaseapp.com",
+  projectId: "smartebm-8ea35",
+  storageBucket: "smartebm-8ea35.firebasestorage.app",
+  messagingSenderId: "94063570937",
+  appId: "1:94063570937:web:512243fbd8fcefc1cfd728",
+  measurementId: "G-T793ST5F56"
+};
+
+export default firebaseConfig;
